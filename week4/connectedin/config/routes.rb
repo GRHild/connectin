135dde8bb0a18ae2c0e94d514d7d2b037d8717ca
Greg Hild <grhild@gmail.com>
2013-09-24 16:29:47 -0500

    Add three people with pictures.

--- week4/connectedin/config/routes.rb
+++ week4/connectedin/config/routes.rb
@@ -1,56 +1,15 @@
 Connectedin::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get '/students/yasmin' => 'students#show_yasmin', as: 'yasmin_students'
+end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+Connectedin::Application.routes.draw do
+  get '/students/justin' => 'students#show_justin', as: 'justin_students'
+end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+Connectedin::Application.routes.draw do
+  get '/students/jae' => 'students#show_jae', as: 'jae_students'
+end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+Connectedin::Application.routes.draw do
+  root 'students#show_all'
 end
