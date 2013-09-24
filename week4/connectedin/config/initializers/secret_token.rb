e1223f67715a0f2c4c64e58721496faf3c9ad49b
Greg Hild <grhild@gmail.com>
2013-09-24 15:25:54 -0500

    Initial commit

---
+++ week4/connectedin/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Connectedin::Application.config.secret_key_base = 'f3994762185c79aca02459b65b1ac39e496b8a2c9a095929f0a9ac9d74d17421d996f056afbe6b1b4d907b0f1247e209e22dde0dcdd808d41342c4a1f05cf083'
