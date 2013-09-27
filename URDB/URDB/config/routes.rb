2062f3b60d79eacbde6be4f96d97419d8a515241
Greg Hild <grhild@gmail.com>
2013-09-27 14:40:07 -0500

    Finished criteria defined by MakerSquare.

--- URDB/URDB/config/routes.rb
+++ URDB/URDB/config/routes.rb
@@ -3,5 +3,4 @@ URDB::Application.routes.draw do
   root 'movies#show_all'
   get '/movies' => 'movies#show_all'
   get '/movies/:id' => 'movies#show', as: 'movie'
-  get '/movies/:youtube_id' => 'movies#show', as: 'trailer'
 end
