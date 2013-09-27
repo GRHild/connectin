63875fa5d84badee085a9d0dbf8800c1db1bcf31
Greg Hild <grhild@gmail.com>
2013-09-27 09:49:49 -0500

    Initial commit

---
+++ URDB/URDB/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URDB::Application.config.secret_key_base = '5c6ad78cec953782f434b6c88b2a44b1f03b3ff6f29a5022aacfe6271c976f73512a85d74cae32d23915dd93ac2ca2b4947759ad397f18006a626acd914b56be'
