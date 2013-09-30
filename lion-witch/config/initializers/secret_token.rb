0ff4234d99c94fbbc5113132438059be46986ab8
Greg Hild <grhild@gmail.com>
2013-09-30 10:02:22 -0500

    Initial Commit

---
+++ lion-witch/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LionWitch::Application.config.secret_key_base = 'ff4eea9588ba2fdbb2a9d70927ad6e9d10d0c6843f959dbc4bc78cce3f0f4768caeba7d5dff02b0b1319e59f259e987bb6bc1105711ba825096ef95f4be99b81'
