b76d93fd0288c6e87d2ff11f35bcdb234e618792
Greg Hild <grhild@gmail.com>
2013-09-30 11:54:33 -0500

    Add Json formatting

--- lion-witch/app/controllers/wardrobe_items_controller.rb
+++ lion-witch/app/controllers/wardrobe_items_controller.rb
@@ -1,5 +1,13 @@
 class WardrobeItemsController < ApplicationController
   def index
     @wardrobe_items = WardrobeItem.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @uwardrobe_items}
+  end
+
+  def show
+    @wardrobe_item = WardrobeItem.find(params[:id])
+
   end
 end
--- lion-witch/spec/controllers/wardrobe_items_controller_spec.rb
+++ lion-witch/spec/controllers/wardrobe_items_controller_spec.rb
@@ -9,4 +9,14 @@ describe WardrobeItemsController do
       expect(assigns('wardrobe_items')).to eq([wardrobe_item])
     end
   end
+
+  describe 'GET show' do
+    it "renders the view of a single wardrobe item" do
+      wardrobe_item = WardrobeItem.create
+      get :show, { :id => wardrobe_item }
+
+      expect(assigns('wardrobe_item')).to eq(wardrobe_item)
+    end
+  end
 end
+
